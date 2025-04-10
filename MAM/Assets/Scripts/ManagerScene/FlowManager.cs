52e05d8a902099ff552c433b8893cb9551ff8cd2
gunmango <iamdoorpark@gmail.com>
2025-04-10 17:48:29 +0900

    tmp랑, 매니저 틀

--- MAM/Assets/Scripts/ManagerScene/FlowManager.cs
+++ MAM/Assets/Scripts/ManagerScene/FlowManager.cs
@@ -1,5 +1,24 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FlowManager : MonoBehaviour
 {
+    //private int _totalDayInWeek = 3;
+    //private int _currentDay = 0;
+    
+    
+    //[SerializeField] private Button _demoNextSceneButton = null;
+
+    
+    
+    private void Start()
+    {
+        //_demoNextSceneButton.onClick.AddListener(ToNextScene);
+    }
+
+    private void ToNextScene()
+    {
+        
+    }
 }
--- MAM/Assets/Scripts/ManagerScene/GameManager.cs
+++ MAM/Assets/Scripts/ManagerScene/GameManager.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
+using System.Collections;
 
+[DefaultExecutionOrder(-1000)]
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
@@ -17,12 +19,24 @@ public class GameManager : MonoBehaviour
     }
     
     [SerializeField] private FlowManager _flowManager = null;
-    [SerializeField] private SceneManager _sceneManager = null;
+    [SerializeField] private SceneController _sceneManager = null;
     [SerializeField] private StudentManager _studentManager = null;
     [SerializeField] private TeamManager _teamManager = null;
 
     public static FlowManager FlowManager => Instance._flowManager;
-    public static SceneManager SceneManager => Instance._sceneManager;
+    public static SceneController SceneManager => Instance._sceneManager;
     public static StudentManager StudentManager => Instance._studentManager;
     public static TeamManager TeamManager => Instance._teamManager;
+
+    public bool IsTestMode = false;
+    
+    private IEnumerator Start()
+    {
+        yield return new WaitUntil(()=> Application.isPlaying);
+        
+        if (IsTestMode)
+            yield break;
+        
+        _sceneManager.LoadTitle();
+    }
 }
