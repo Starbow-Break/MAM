93c3f330bf056c6c3456f3c60e487fbe841e04c3
STARBOW <starbow.dev@gmail.com>
2025-04-16 13:17:17 +0900

    ADD : 식당 목록 받아오기 구현

---
+++ MAM/Assets/Scripts/LunchScene/Setter/LunchRestaurantButtonSetter.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class LunchRestaurantButtonSetter : MonoBehaviour
+{
+    
+}
---
+++ MAM/Assets/Scripts/ManagerScene/Restaurant/RestaurantTable.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+[Serializable]
+public struct Restaurant
+{
+    public string ID;
+    
+    public string Name;
+    public Sprite Icon;
+}
+
+
+[CreateAssetMenu(fileName = "RestaurantTable", menuName = "Scriptable Object/RestaurantTable")]
+public class RestaurantTable : ScriptableObject
+{
+#if UNITY_EDITOR
+    public string URL_ID = "1geUI-yXY5iO3cqlfqMH_vRGQ5TQjIyalk7tYtBUhAVA";
+    public string URL_SHEET = "1049994877";
+#endif
+    
+    public List<Restaurant> RestaurantList = new List<Restaurant>();
+    
+    public Restaurant GetRestaurant(string ID)
+    {
+        return RestaurantList.Find(x => x.ID == ID);
+    }
+
+    public List<Restaurant> GetRestaurants()
+    {
+        return RestaurantList;
+    }
+
+    public int GetTotalRestaurantCount()
+    {
+        return RestaurantList.Count;
+    }
+}
+
